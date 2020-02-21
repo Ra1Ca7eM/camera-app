2ca5a483c23306187cbbae724c66dabca4772902
Ra1Ca7eM <61329084+Ra1Ca7eM@users.noreply.github.com>
2020-02-21 21:58:16 +0100

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ var constraints = { video: { facingMode: "user" }, audio: false };
 const cameraView = document.querySelector("#camera--view"),
     cameraOutput = document.querySelector("#camera--output"),
     cameraSensor = document.querySelector("#camera--sensor"),
-    cameraTrigger = document.querySelector("#camera--trigger")
+    cameraTrigger = document.querySelector("#camera--trigger"),
+    cameraSwitch = document.querySelector("#switchCameraButton")
 // Access the device camera and stream to cameraView
 function cameraStart() {
     navigator.mediaDevices
@@ -25,5 +26,21 @@ cameraTrigger.onclick = function() {
     cameraOutput.src = cameraSensor.toDataURL("image/webp");
     cameraOutput.classList.add("taken");
 };
+
+cameraSwitch.onClick = function(){
+
+if (amountOfCameras > 1) {
+    switchCameraButton.style.display = 'block';
+
+    switchCameraButton.addEventListener('click', function() {
+      if (currentFacingMode === 'environment') currentFacingMode = 'user';
+      else currentFacingMode = 'environment';
+
+      cameraTrigger();
+    });
+  }
+}
+
+
 // Start the video stream when the window loads
 window.addEventListener("load", cameraStart, false);
